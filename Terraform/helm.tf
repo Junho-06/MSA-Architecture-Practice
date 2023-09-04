8820f8e64337d5694a85cb59b31549948dc747d1
Junho-06 <k34920032@gmail.com>
2023-09-04 17:23:20 +0900

    update :: argocd namespace

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -1,8 +1,9 @@
 locals {
   helm-repository = "https://junho-06.github.io/MSA-Architecture-Practice/helm-charts"
 
-  argocd-name     = "argo-cd"
-  argocd-version  = "5.45.0"
+  argocd-name      = "argo-cd"
+  argocd-version   = "5.45.0"
+  argocd-namespace = "argocd"
 }
 
 resource "helm_release" "argo-cd" {
@@ -10,4 +11,7 @@ resource "helm_release" "argo-cd" {
   repository = local.helm-repository
   chart      = local.argocd-name
   version    = local.argocd-version
+  
+  namespace        = local.argocd-namespace
+  create_namespace = true
 }

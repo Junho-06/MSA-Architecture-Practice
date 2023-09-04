64e1fcf5e6aff7fade2ddc53522da89ff36f7086
Junho-06 <k34920032@gmail.com>
2023-09-04 15:54:44 +0900

    update :: terraform file tree

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -14,3 +14,18 @@ provider "helm" {
     token                  = aws_eks_cluster.msa_eks_cluster.cluster_auth_token
   }
 }
+
+
+locals {
+  helm-repository = "https://junho-06.github.io/MSA-Architecture-Practice/helm-charts"
+
+  argocd-name     = "argo-cd"
+  argocd-version  = "5.45.0"
+}
+
+resource "helm_release" "argo-cd" {
+  name       = local.argocd-name
+  repository = local.helm-repository
+  chart      = local.argocd-name
+  version    = local.argocd-version
+}

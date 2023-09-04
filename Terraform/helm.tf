22e81aafb5705e7754e32af2c8ea167c9ed851d9
Junho-06 <k34920032@gmail.com>
2023-09-04 16:38:48 +0900

    update :: provider

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.11.0"
-    }
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.msa_eks_cluster.endpoint
-    cluster_ca_certificate = aws_eks_cluster.msa_eks_cluster.cluster_ca_certificate
-    token                  = aws_eks_cluster.msa_eks_cluster.cluster_auth_token
-  }
-}
-
-
 locals {
   helm-repository = "https://junho-06.github.io/MSA-Architecture-Practice/helm-charts"
 
--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "5.15.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.11.0"
+    }
   }
   cloud {
     hostname     = "app.terraform.io"
@@ -11,6 +15,14 @@ terraform {
     workspaces {
       name = "msa-practice"
     }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.msa_eks_cluster.endpoint
+    cluster_ca_certificate = aws_eks_cluster.msa_eks_cluster.cluster_ca_certificate
+    token                  = aws_eks_cluster.msa_eks_cluster.cluster_auth_token
   }
 }
 

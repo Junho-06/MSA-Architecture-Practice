d28dae2f586db64fda46e4d6d6f0ddf187036193
Junho-06 <k34920032@gmail.com>
2023-09-04 10:22:17 +0900

    update :: ecr registry -> Go / Spring

--- Terraform/ecr.tf
+++ Terraform/ecr.tf
@@ -1,7 +1,15 @@
-resource "aws_ecr_repository" "msa_ecr" {
-  name                 = "msa_ecr_registry"
+resource "aws_ecr_repository" "msa_ecr_go" {
+  name                 = "msa_ecr_go_registry"
 
   tags = {
-    Name = "msa_ecr_registry"
+    Name = "msa_ecr_go_registry"
+  }
+}
+
+resource "aws_ecr_repository" "msa_ecr_spring" {
+  name                 = "msa_ecr_spring_registry"
+
+  tags = {
+    Name = "msa_ecr_spring_registry"
   }
 }

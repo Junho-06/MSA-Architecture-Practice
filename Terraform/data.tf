edfc1a34472c5b22b7281e08c028911ac8b220f2
Junho-06 <k34920032@gmail.com>
2023-09-10 23:39:13 +0900

    delete :: useless file

--- Terraform/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_eks_cluster" "cluster" {
-  name = aws_eks_cluster.msa_eks_cluster.name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.msa_eks_cluster.name
-}
--- Terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type        = string
-  description = "default region"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "eks cluster name"
-}

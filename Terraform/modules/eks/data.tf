e5953790c6b0b48187656c030eb501b33ea1b5e7
Junho-06 <k34920032@gmail.com>
2023-09-12 01:34:58 +0900

    update :: data cluster_name

--- Terraform/modules/eks/data.tf
+++ Terraform/modules/eks/data.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_caller_identity" "current" {}

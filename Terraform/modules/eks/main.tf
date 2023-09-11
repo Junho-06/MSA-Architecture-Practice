c269fb2781f88906eee68d24923354432739866e
Junho-06 <k34920032@gmail.com>
2023-09-11 19:58:34 +0900

    update :: additional_policies string mapping

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -41,9 +41,9 @@ module "eks" {
       max_size     = var.nodegroup_max_size
       desired_size = var.nodegroup_desired_size
 
-      iam_role_additional_policies = [
-        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      ]
+      iam_role_additional_policies = {
+        ssm_managed_instance_core = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+      }
     }
   }
 

9edf48497cbecf7f07ed5319fa261c3ca26f0f7e
Junho-06 <k34920032@gmail.com>
2023-09-11 19:48:20 +0900

    update :: delete launch_tempalte name field

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -36,7 +36,6 @@ module "eks" {
       instance_types         = [local.instance_type]
       create_security_group  = false
       create_launch_template = false
-      launch_template_name   = ""
 
       min_size     = var.nodegroup_min_size
       max_size     = var.nodegroup_max_size

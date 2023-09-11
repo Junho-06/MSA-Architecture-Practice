b8b34d9568fe6041f463175ef0477133382c9346
Junho-06 <k34920032@gmail.com>
2023-09-11 18:10:19 +0900

    update :: divide provider

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -22,17 +22,3 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}

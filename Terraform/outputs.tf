00ed39370b224bb1f65a57ac5f9647e182a846fd
Junho-06 <k34920032@gmail.com>
2023-09-13 22:41:04 +0900

    delete :: output ecr url

--- Terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "go_ecr_repository_url" {
-  value = aws_ecr_repository.msa_ecr_go.repository_url
-}
-
-output "spring_ecr_repository_url" {
-  value = aws_ecr_repository.msa_ecr_spring.repository_url
-}

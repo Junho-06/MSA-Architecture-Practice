cb67dc8babbccc1de27b564f24c5ceed95551922
Junho-06 <k34920032@gmail.com>
2023-09-16 12:14:23 +0900

    test :: Go build test

--- Go/main.go
+++ Go/main.go
@@ -16,7 +16,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	resp := make(map[string]string)
-	resp["message"] = "Http Status OK"
+	resp["message"] = "OK"
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
 		log.Fatalf("Error happened in JSON marshal. Err: %s", err)

7913481ea074fc36d047abf6f37b85f45cb0d54e
Junho-06 <k34920032@gmail.com>
2023-09-21 20:08:05 +0900

    update :: ecr module force_delete true

--- Terraform/modules/ecr/main.tf
+++ Terraform/modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "repo" {
   name                 = var.name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = var.is_scan_on_push

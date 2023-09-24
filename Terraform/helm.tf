08a95e5b2d677652adf956fbff50c5bb36195cc6
Junho-06 <k34920032@gmail.com>
2023-09-24 12:07:55 +0900

    delete :: applicatio role

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -14,7 +14,7 @@ locals {
   argocd-namespace = "argocd"
 
   application-name      = "application"
-  application-version   = "0.1.0"
+  application-version   = "0.1.1"
 
   aws-load-balancer-controller-name      = "aws-load-balancer-controller"
   aws-load-balancer-controller-version   = "1.6.2"

4ae57c46d7146d3926bd39bfe15ddf5d0ba0ef94
Junho-06 <k34920032@gmail.com>
2023-09-26 23:25:33 +0900

    update :: subnet tag move vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -14,4 +14,14 @@ module "vpc" {
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
   name_prefix     = local.name_prefix
+
+  private_subnet_tags = {
+  # 이부분 에러남 이게 되야 ingress 생성 될듯
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  public_subnet_tags = {
+  # 이부분 에러남 이게 되야 ingress 생성 될듯
+    "kubernetes.io/role/elb" = "1"
+  }
 }

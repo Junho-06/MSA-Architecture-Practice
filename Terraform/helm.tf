387ce6eff697b669141ba765f58307f632b92a09
Junho-06 <k34920032@gmail.com>
2023-09-30 13:40:50 +0900

    update :: argocd ingress service port

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -10,7 +10,7 @@ locals {
   helm-repository = "https://junho-06.github.io/MSA-Architecture-Practice"
 
   argocd-name      = "argo-cd"
-  argocd-version   = "0.1.16"
+  argocd-version   = "0.1.17"
   argocd-namespace = "argocd"
 
   application-name      = "application"

273e8ebc9abfe020413a7d5afe7299f288dfccd8
Junho-06 <k34920032@gmail.com>
2023-10-05 10:18:24 +0900

    update :: application chart version up

--- Terraform/helm.tf
+++ Terraform/helm.tf
@@ -14,7 +14,7 @@ locals {
   argocd-namespace = "argocd"
 
   application-name      = "application"
-  application-version   = "0.1.4"
+  application-version   = "0.1.5"
 
   aws-load-balancer-controller-name      = "aws-load-balancer-controller"
   aws-load-balancer-controller-version   = "1.6.4"
